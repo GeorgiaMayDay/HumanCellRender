57cf112d44020226fa8d39c18f3d527faf680b60
Georgia May Cawley <georgia.cawley@student.manchester.ac.uk>
2024-02-01 17:49:45 +0000

    Editting config file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
     base: "https://georgiamayday.github.io/HumanCellRender/",
     publicPath: process.env.NODE_ENV === 'production' ?
         '/HumanCellRender/' : '/'
