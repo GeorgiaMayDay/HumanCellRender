f62b199452b76ec5d3cdcb466f63538b43c1865c
Georgia May Cawley <georgia.cawley@student.manchester.ac.uk>
2024-02-11 13:15:03 +0000

    Spelling mistakes

--- cell_model/descriptions.js
+++ cell_model/descriptions.js
@@ -9,37 +9,37 @@ export const mitochondria_adv = "<p> Mitochondria are membrane-bound cell organe
     "ATP is an energy source, because the phosphate (that the P in ATP) is a high-energy bond and if broken, releases energy that can be used for other reactions within the cell. </p>" +
     " <p> Some different cells have different amounts of mitochondria because they need more energy. So for example, the muscle has a lot of mitochondria, the liver does too, the kidney as well, and to a certain extent, the brain, which lives off of the energy those mitochondria produce. </p><p> So if you have a defect in the pathways that the mitochondria usually functions with, you're going to have symptoms in the muscle, in the brain, sometimes in the kidneys as well; many different types of symptoms. And we probably don't know all of the different diseases that mitochondrial dysfunction causes.</p>";
 
-export const cell_membrane_basic = "Selectively permeable double layer of lipid (lipid bilayer) with protien molcules dotted within it.  It acts as a border for the cell and the outside world and like a border guard controls what substance can enter and leave the cell. It is present in all cells.";
+export const cell_membrane_basic = "Selectively permeable double layer of lipid (lipid bilayer) with protein molcules dotted within it.  It acts as a border for the cell and the outside world and like a border guard controls what substance can enter and leave the cell. It is present in all cells.";
 
 export const cell_membrane_adv = "<p> The cell membrane (or plasma membrane) is found in all cells and separates the interior of the cell from the outside. This membrane is made up of a semi-permeable lipid bilayer. It regulates what materials enter and leave the cell </p>" +
-    "<p> It also manages how cells communciate with other cells, through what chemicals it realises and through certain protiens are studded through out. </p>" +
-    "<p> Chemicals move through across the membrane with through diffusion, the movement of molecules from an area of high concentration to an area of low concentration, or through transport protiens which move bigger molecules in and out. </p>";
+    "<p> It also manages how cells communciate with other cells, through what chemicals it realises and through certain proteins are studded through out. </p>" +
+    "<p> Chemicals move through across the membrane with through diffusion, the movement of molecules from an area of high concentration to an area of low concentration, or through transport proteins which move bigger molecules in and out. </p>";
 
-export const nucleolus_basic = "Part of the nucleus, the nucleolus constructs the subunits that will make up ribosomes. Ribsomal sub-units are built out of ribosomal RNA, a nucleric acid like DNA, and protiens . </p> " +
+export const nucleolus_basic = "Part of the nucleus, the nucleolus constructs the subunits that will make up ribosomes. Ribsomal sub-units are built out of ribosomal RNA, a nucleric acid like DNA, and proteins . </p> " +
     " <p> These subunits are then moved into the larger nucleus where they are fused into ribsomes, before being released into the cytoplasm. </p>";
 
 export const nucleolus_adv = "<p> Part of the nucleus, the nucleolus is the largest structure in the nucleus. It's main role is to produce ribosomal subunits. </p>" +
-    "<p> Ribosomal subunits combine in pairs to create ribosomes, though that process occurs outside the nucleolus. These ribosomal subunits are contructed of ribosomal RNA (rRNA) and protiens</p>" +
+    "<p> Ribosomal subunits combine in pairs to create ribosomes, though that process occurs outside the nucleolus. These ribosomal subunits are contructed of ribosomal RNA (rRNA) and proteins</p>" +
     "<p> Nucleolus also contributes to other roles like cell growth management but those area's are still under research</p>";
 
 // "<p> Part of the nucleus, the nucleolus is the largest structure in the nucleous and form around specific chomosomal regions (organising regions). The Nucleolus is made of proteins, DNA and RNA. </p>" +
 // " <p>RNA is a nucleric acid like DNA, but smaller. Importantly unlike DNA, is able to leave the nucleus. This means that it can carry messages from the nucleous to other organelles </p>" +
-// "<p> RNA is created by protiens that copying DNA into RNA, a process known as transcription. This forms m-RNA which is then further processed within the nucleous before moving into the cytoplasm and entering a ribosome. The ribosome then creates the protien encoded onto the RNA, in a process known as protien synthesis </p>";
+// "<p> RNA is created by proteins that copying DNA into RNA, a process known as transcription. This forms m-RNA which is then further processed within the nucleous before moving into the cytoplasm and entering a ribosome. The ribosome then creates the protein encoded onto the RNA, in a process known as protein synthesis </p>";
 
 export const lysosome_basic = "<p> Lysosomes act the digestive and disposal system of the cell. There are normally multiple in a cell. </p> <p> They contains a variety of enzymes that each breakdown the different waste materials and cellular debris. </p><p> They are a type of vesicle - which are membrane bound structures </p>";
 
 //Needs more work
-export const lysosome_adv = "<p> Lysosomes are membrane-enclosed organelles that act the digestive system of the cell. They breakdown protiens, nucleic acids(DNA and RNA), carbohydrates, and lipids. </p> <p>Although portrayed as a sphere here, they actually come in a variety of shapes and sizes, due to being only defined by being a membrane-bound structure filled with digestive enzymes </p>.";
+export const lysosome_adv = "<p> Lysosomes are membrane-enclosed organelles that act the digestive system of the cell. They breakdown proteins, nucleic acids(DNA and RNA), carbohydrates, and lipids. </p> <p>Although portrayed as a sphere here, they actually come in a variety of shapes and sizes, due to being only defined by being a membrane-bound structure filled with digestive enzymes </p>.";
 
-export const golgi_body_basic = "<p> The Golgi body, also known as the Golgi Apparatus, is an organelle made of folded membrane sacs. It is involved in protien processing. </p>" +
+export const golgi_body_basic = "<p> The Golgi body, also known as the Golgi Apparatus, is an organelle made of folded membrane sacs. It is involved in protein processing. </p>" +
     "<p> Protiens and other molecules, leave the Endoplasmic Recticulum and the Golgi Body absorbs them. The Golgi Body adds any more molecules, if needed, and wraps them in a membrane sac (known as a vesicles). </p>" +
-    "<p>The protiens, in their vesciles can then can be moved to their final destination </p>";
+    "<p>The proteins, in their vesciles can then can be moved to their final destination </p>";
 
 export const golgi_body_adv = "<p> The Golgi Body, or Golgi Apparatus, is a made up of a series of stacked, folded membranes. These flattened sacs are known as cisternae. It is located next to the nucleus, in what's known as the perinuclear position. </p>" +
-    "<p> The primary function of the Golgi Body is processing and sorting protiens, though it does also sort lipids in a similar manner. </p>" +
-    "<p> Processing means modifying the protien by adding other materials. For example, it could add carbohydrates to a protein, creating a composite known as a glycoprotien. </p>" +
-    "<p> Once the products have been processed, they get wrapped in a vesicle, a membrane sac. The type of vesicle the product is wrapped in tells the cells where to send the protien. </p> " +
-    "<p> For example, protiens or materials, that are meant to leave the cell are wrapped in secretory vesicles so the cell knows they're mean to leave.  </p>" +
+    "<p> The primary function of the Golgi Body is processing and sorting proteins, though it does also sort lipids in a similar manner. </p>" +
+    "<p> Processing means modifying the protein by adding other materials. For example, it could add carbohydrates to a protein, creating a composite known as a glycoprotein. </p>" +
+    "<p> Once the products have been processed, they get wrapped in a vesicle, a membrane sac. The type of vesicle the product is wrapped in tells the cells where to send the protein. </p> " +
+    "<p> For example, proteins or materials, that are meant to leave the cell are wrapped in secretory vesicles so the cell knows they're mean to leave.  </p>" +
     "<p> This process is how the vast majority of enzymes, horomones and ion channels are made. </p>";
 
 export const rough_ER_basic = "<p> The Rough Endoplasmic Recticulum (Rough) is part of the Endoplasmic Recticulum (ER) - the largest organelle in the cell. </p>" +
@@ -47,16 +47,16 @@ export const rough_ER_basic = "<p> The Rough Endoplasmic Recticulum (Rough) is p
     " <p> The most important function of the Rough ER is protein synthesis. The folded membrane forms channels within its self, and using the ribosomes within the membrane, proteins are generated. </p>";
 
 export const rough_ER_adv = "<p>The Endoplasmic Recticulum (ER) is the largest organelle in the human cell, often accounting for 10% of the cells volume. However the ER is shifting in shape and size, so this is not always true - even in the same cell </p>" +
-    "<p> The  Endoplasmic Recticulum is made up of a series of cistarne (sacs of membrane), folded over each other to create channels. The membrane is not just a lipid bilayer containing with ribosomes and protiens. These ribosomes and proteins are both part of the process of protien synthesis </p>" +
-    "<p> The Rough ER is essential for protien synthesis in mammalian cells, even protiens produced by free-floating ribosomes must go through the Rough ER before being used. Within the Rough ER, these proteins are processed further, which tells them which organelle they should go to next. </p>";
+    "<p> The  Endoplasmic Recticulum is made up of a series of cistarne (sacs of membrane), folded over each other to create channels. The membrane is not just a lipid bilayer containing with ribosomes and proteins. These ribosomes and proteins are both part of the process of protein synthesis </p>" +
+    "<p> The Rough ER is essential for protein synthesis in mammalian cells, even proteins produced by free-floating ribosomes must go through the Rough ER before being used. Within the Rough ER, these proteins are processed further, which tells them which organelle they should go to next. </p>";
 
 export const smooth_ER_basic = "<p> The Smooth Endoplasmic Recticulum is part of the Endoplasmic Recticulum (ER) - the largest organelle in the cell. It is made up of a number of tubular, interconnected, membranes. </p>" +
-    "<p> While the Rough ER is focused on protien processing, the Smooth ER is focused on lipid synthesis. These lipids are used to construct and repair membranes across the cell. </p>";
+    "<p> While the Rough ER is focused on protein processing, the Smooth ER is focused on lipid synthesis. These lipids are used to construct and repair membranes across the cell. </p>";
 
 export const smooth_ER_adv = "<p> Part of the Endoplasmic Recticulum, the Smooth ER is different to the Rough in shape and function. </p>" +
     "<p> It's does not have any ribosomes on the surface, hence being called smooth, and is more tubular. The different look follows from it performing a different function to the Rough ER.</p>" +
     "<p> The primary role of the Smooth ER is to synthesis lipids. These lipids must be synthesised within a membrane as they are hydrophobic i.e repel water. These lipids are essential to building and maintaining membranes, both for organelle and cell itself. These lipids also used in other biological reaction. </p>" +
-    "<p> After these lipids are created within the Smooth ER, they like the protiens are exported to the Golgi Body </p>";
+    "<p> After these lipids are created within the Smooth ER, they like the proteins are exported to the Golgi Body </p>";
 
 export const centrioles_adv = "<p> This pair of organelles are used in mitosis and meiosis, to pull the cell apart. </p>" +
     "<p> When they are not involved to dividing, they sit as they do now, at right angles to each other and make up part of the centrosome. The centrosome provides structure and support to the cell by producing and organising microtubules.  </p>" +
@@ -64,17 +64,17 @@ export const centrioles_adv = "<p> This pair of organelles are used in mitosis a
 
 export const centrioles_basic = "<p> This pair of organelles are used in mitosis and meiosis, to pull the cell apart. Collectively </p>" + "<p>Mitosis is the division of a parent cell into daughter cells. These daughter cells are genetical identical to each other and to the parent cell</p><p> Meiosis is when a parent cell splits into four genetical different hapliod (half the genes) cells. Meiosis is only done by reproductive cells to create gametes.  </p>";;
 
-export const nuclear_envelope_basic = "<p> A complex membrane around the nucleus. Molecules can only pass through the nuclear pore complexes, the circluar holes surrounded by protiens in the membrane.</p>" +
+export const nuclear_envelope_basic = "<p> A complex membrane around the nucleus. Molecules can only pass through the nuclear pore complexes, the circluar holes surrounded by proteins in the membrane.</p>" +
     "<p> Because molecules and RNA must pass through the nuclear pores to pass through the nuclear envelope, it is able to control what is able to leave and enter the nucleous </p>";
 
 export const nuclear_envelope_adv = "<p> The nuclear enveolope is a multi-layer membrane, which seperaete the nucleus from the cytoplasm. Acting as this barrier, it is able to regulates what comes in and out of the nucleus </p>" +
     "<p> The nuclear enveolope has two lipid bilayers, known as the outer nuclear membrane (ONM) and the inner nuclear membrane (IMN). These membrane are distinct from each other and provide different functionality for the nucleus. </p>" +
-    "<p> The outer membrane can connects to the rough Endoplasmic Recticulum and has ribsomes studded within it. The inner nuclear membrane contains protiens that support the inner workings for the nucleus. </p>" +
+    "<p> The outer membrane can connects to the rough Endoplasmic Recticulum and has ribsomes studded within it. The inner nuclear membrane contains proteins that support the inner workings for the nucleus. </p>" +
     "<p> These two membranes are fused at the nuclear pore complexes, holes ringed by proteins in the membrane. This allows the nuclear pores to penetrate entirely through the nuclear envelope</p>" +
     "<p> RNA and larger molecules can only pass through the nuclear pore complexes, so nuclear pores how the nuclear envelope controls what comes out the cell</p>";
 
 export const cytsol_basic = "<p> Cytsol is the liquid part of the Cytoplasm, a term used for the space between the cell membrane and the nucleus. The fluid maintains the shape of the cell and fills the void between organelle</p>" +
-    "<p> Ribsomes, organelle, protien, all float within it. Millions of chemical reactions happen within it, as it helps ferry molecules, protiens and enzymes around. </p>";
+    "<p> Ribsomes, organelle, protein, all float within it. Millions of chemical reactions happen within it, as it helps ferry molecules, proteins and enzymes around. </p>";
 
 export const cytsol_adv = " <p> Cytsol is the liquid in cell, seperated from organelle and contained by membranes. </p>" +
     "<p> It is not the same as the Cytoplasm which is everything between the nucleus and the cell membrane - so includes organelles like the mitochondria. </p>"
@@ -90,11 +90,11 @@ export const nucleus_adv = "<p> The container and organiser of chromatin, the mi
     "<p> Within the nucleus, there are sub-structures dedicated to performing different internal functions - for example, the nuceleolus which constructs ribosomes.  </p>" +
     "<p> The nucleus is contained by the nuclear envelope, which protects it, regulates the flow of materials, and provides structure for the nucleus. </p>";
 
-export const ribosome_basic = "<p> These protien complexes are where protiens are made. This why most of them are found in the membrane of the  Rough ER, the organelle that is the first stage in protien processing. </p> <p> They recieve messenger RNA from the nucleus and read it, in a process called translation. </p><p>The RNA tells them what order they should connect amino acid together to make a protien.</p> <p> They are created within the nucleus </p>";
+export const ribosome_basic = "<p> These protein complexes are where proteins are made. This why most of them are found in the membrane of the  Rough ER, the organelle that is the first stage in protein processing. </p> <p> They recieve messenger RNA from the nucleus and read it, in a process called translation. </p><p>The RNA tells them what order they should connect amino acid together to make a protein.</p> <p> They are created within the nucleus </p>";
 
-export const ribosome_adv = "<p> Ribosomes are molecular machines responsible for the creation of protiens from amino acids </p> " +
+export const ribosome_adv = "<p> Ribosomes are molecular machines responsible for the creation of proteins from amino acids </p> " +
     "<p> Ribsomes are made up of two sub-units that process messenger RNA (mRNA) in the gap between. It then matches the mRNA code with the appriopriate amino acid. </p>" +
     "<p> It does this by matching sections of the mRNA code, 3 letter sequences called codons, to transfer RNA (tRNA), a short nucelic acid with a matching amino acid attached </p>" +
-    "<p> The ribsome takes the amino acid off the tRNA and adds it the protien string. This process goes on til the ribosome hits the stop codon - a specific sequence at the end of all mRNA </p>" +
+    "<p> The ribsome takes the amino acid off the tRNA and adds it the protein string. This process goes on til the ribosome hits the stop codon - a specific sequence at the end of all mRNA </p>" +
     "<p> This process is known as translation and it is the foundational process from which all proteins, enzymes, ion channels and horomones are built from. </p>" +
     "<p> Ribsomes are created in the nuleolus, within the nucleus. </p>";
